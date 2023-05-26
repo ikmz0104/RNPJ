020071d0d3644953891a88d3cf276fdeab7a84b9
k.ikemizu <k.ikemizu@questry.co.jp>
2023-05-26 21:38:31 +0900

    wip: init

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 
 const HelloWorld = () => {
-  return (
-    <View>
-      <Text>Hello, World!</Text>
-    </View>
-  );
+  return <></>;
 };
 
 export default HelloWorld;
