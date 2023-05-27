ca3ea4c2c409dd6504b020ae8fc80f9a5429203a
k.ikemizu <k.ikemizu@questry.co.jp>
2023-05-27 13:41:25 +0900

    fix

--- src/components/orgnisms/ArumiCard/index.tsx
+++ src/components/orgnisms/ArumiCard/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {View, Text, Image} from 'react-native';
-import ImageOverlay from 'react-native-image-overlay';
 import styles from './index.style';
 
 type ArumiCardItemProps = {
