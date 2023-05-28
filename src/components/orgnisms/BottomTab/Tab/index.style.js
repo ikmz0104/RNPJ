5935e3e40fcdfd0153dbdb7241edd99b1fdc971a
k.ikemizu <k.ikemizu@questry.co.jp>
2023-05-28 18:12:19 +0900

    おk

---
+++ src/components/orgnisms/BottomTab/Tab/index.style.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: 'aqua',
+    padding: 10,
+  },
+  text: {
+    color: 'white', // テキストの色を白色に設定
+    fontWeight: 'bold', // テキストを太字に設定
+  },
+});
